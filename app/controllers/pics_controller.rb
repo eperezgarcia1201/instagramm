72cd5564590fb9942e09462b917f9de11b872273
Elmer <admin@aprode.org>
2016-11-01 16:21:31 -0500

    Added acts_as_votable gem and setting up

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -1,5 +1,6 @@
 class PicsController < ApplicationController
-	before_action :find_pic, only: [:show, :edit, :update, :destroy]
+	before_action :find_pic, only: [:show, :edit, :update, :destroy, :upvote]
+	before_action :authenticate_user!, except: [:index, :show]
 	
 
 	def index
@@ -43,7 +44,10 @@ class PicsController < ApplicationController
 
 	end
 
-
+	def upvote
+		@pic.upvote_by current_user
+		redirect_to :back
+	end
 	private
 
 	def pic_params
